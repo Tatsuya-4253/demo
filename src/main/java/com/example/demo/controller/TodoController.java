cc202b099c64d1b5e1de378cd0325c1ef6ab340f
Tatsuya-4253 <kakuhsan25@gmail.com>
2026-09-06 01:38:06 +0900

    CURD機能修正

--- src/main/java/com/example/demo/controller/TodoController.java
+++ src/main/java/com/example/demo/controller/TodoController.java
@@ -99,7 +99,6 @@ public class TodoController {
         updatetodo.setName(name);
         updatetodo.setStyle(style);
         todoRepository.save(updatetodo);
-
         return "redirect:/todo";
     }
 
